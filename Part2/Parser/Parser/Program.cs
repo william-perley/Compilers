368b8f52b322debc903e02c0398c2a748bd5ad53
william-perley <n00636615@ospreys.unf.edu>
2017-10-10 19:02:36 -0400

    Trying to make globals

--- Part2/Parser/Parser/Program.cs
+++ Part2/Parser/Parser/Program.cs
@@ -7,27 +7,63 @@ namespace Parser
 {
     class Program
     {
+        int x = 0;
+        string[] lines = System.IO.File.ReadAllLines(@"./tokens.txt");
+        
         static void Main(string[] args)
         {
-
-            FileBeingRead file = new FileBeingRead();
-            file.getFile();
-
+            Program Parser = new Program();
+            //FileBeingRead file = new FileBeingRead();
+            //file.getFile();
+            //Console.WriteLine(file.nextToken()); 
+            Console.WriteLine("The first token is " + Parser.nextToken());
+            Console.WriteLine("The second token is " + Parser.nextToken());
+            
         }
-        
-
-    }
-   
-
-    class FileBeingRead
-    {
-        public void getFile()
+       
+        public void testing()
         {
-            string[] lines = System.IO.File.ReadAllLines(@"./tokens.txt");
-            Console.WriteLine(lines[0]);
+            
+            
+          //  Console.WriteLine("Next token is " +y);
         }
+        public string nextToken()
+        {
+            while ((lines[x] == null) || (lines[x] == ""))
+            {
+                x++;
+            }
+            return lines[x++];
+        }
+            
     }
+    
+
 }
+public static class globals
+{
+
+}
+
+//class FileBeingRead
+//{
+    
+
+//    public void getFile()
+//    {
+        
+//        Console.WriteLine(lines[4]);
+//    }
+
+//    public string nextToken()
+//    {
+//        x++;
+//        return lines[++x]; 
+        
+//    }
+
+//}
+
 
 
 
