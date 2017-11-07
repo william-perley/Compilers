3c34b8e165ef47fb2f50d307fab9621f27dcbac7
william-perley <n00636615@ospreys.unf.edu>
2017-11-07 00:37:27 -0500

    Changed to List of Lists

--- LinkedListTest/LinkedListTest/Program.cs
+++ LinkedListTest/LinkedListTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LinkedListTest
 {
@@ -8,32 +9,130 @@ namespace LinkedListTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            SymbolTable list = new SymbolTable();
+
+            list.AddToken("this");
+            list.AddToken("that");
+            list.AddNewDepth();
+            list.AddToken("this1");
+            list.AddToken("that1");
+            list.AddNewDepth();
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
-    class LinkedList
-    {
-        int currentNode = 0;
-        Link[] funcitonList = new Link[20];
 
-        public void AddNewList(string id, string type)
+    
+    class SymbolTable
+    {
+        int currentNodeIndex = 0;
+        List<List<string>> depth = new List<List<string>>();
+        List<string> tokens = new List<string>();
+
+        public void AddNewDepth()
         {
-            string stringToAdd = id + " " + type;
-         
-            
-            
-            
+
+            depth.Add(new List<string>(tokens));
+            tokens.Clear();
+            currentNodeIndex++;
+           
+
         }
+
+        public void GoUpDepth()
+        {
+            depth.RemoveAt(currentNodeIndex);
+            currentNodeIndex--;
+            tokens.Clear();
+            tokens = depth[currentNodeIndex];
+        }
+
+        public void AddToken(string token)
+        {
+            tokens.Add(token);
+        }
+
+        public bool IsTokenDeclared(string token)
+        {
+            foreach(List<string> t in depth)
+            {
+                if (t.Contains(token))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool DoesTokenExist(string token)
+        {
+            if (tokens.Contains(token))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsTokenSameType(string typeOne, string typeTwo)
+        {
+
+            return false;
+        }
+
+        public void CurrentTokenList()
+        {
+            foreach(string t in tokens)
+            {
+                Console.WriteLine("Token in Tokens is " + t);
+            }
+        }
+
+            //var myLoL = new List<List<string>>();
+
+            //List<string> cNode = myLoL[cNodeIndex];
+            //myLoL.RemoveAt(cNodeIndex);
+            //cNode = myLoL[cNodeIndex];
+            //cNode.add
+
+
+            //var inner = new List<string>();
+            //inner.Add("afjiopf");
+            //myLoL.Add(inner);
+
+            //myLoL.Add(inner);
+
+            //myLoL.Add(inner);
+
+            //myLoL.Add(inner);
+
+            //int count = myLoL.Count;
+            //for (int i=0; i<count; i++)
+            //{
+                
+            //}
+
+            //var myEnumerator = myLoL.GetEnumerator();
+            //while (myEnumerator.MoveNext())
+            //{
+            //    if (myEnumerator.Current.Contains("awjief"))
+            //    {
+
+            //    }
+            //}
+
+
+            //foreach (string innerList in myLoL)
+            //{
+            //    if (innerList.Contains("myStrofjaepje"))
+            //    {
+            //        // jefaop break; return true;
+            //    }
+            //}
+
+
+
         
     }
-    class Link
-    {
-        List<string> inputAndType = new List<string>();
-
-        public void AddToList(string input)
-        {
-            
-            inputAndType.Add(input);
-        }
-    }
+       
 }
